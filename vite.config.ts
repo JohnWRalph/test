0fb3a8ecb77e1672c800f5c4b46a8b26314f1d91
JohnWRalph <John.Ralph.Git@gmail.com>
2023-01-28 22:19:57 -0600

    added potential fix for buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
-import {Buffer} from "buffer"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), nodePolyfills()],
