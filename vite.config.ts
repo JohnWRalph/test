5f58f8a8e0b8e6bdcd91ae47d9304b02e81d99eb
JohnWRalph <John.Ralph.Git@gmail.com>
2023-01-28 21:36:29 -0600

    added potential fix for buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
+import {Buffer} from "buffer"
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), nodePolyfills()],
-  define: {global: 'window','process.env.ANCHOR_BROWSER': true, Buffer: "buffer"}, 
+  plugins: [svelte()],
+  define: {global: 'window','process.env.ANCHOR_BROWSER': true}, 
+  optimizeDeps: {
+    esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                buffer: true
+            })
+        ]
+    }
+}
 })
 
 
